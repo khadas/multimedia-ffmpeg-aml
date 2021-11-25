2f89a89584db15dc185103d42c7eebe12811a2fc
kenan.liu <kenan.liu@amlogic.com>
2021-11-25 10:43:32 +0800

    ffmpeg: CB0 compat for linux [1/1]
    PD#SWPL-57554
    
    Problem:
    compat for linux
    
    Solution:
    distinguish with __ANDROID__
    
    Verify:
    AP222
    
    Change-Id: I3d2f6040a6497a2136a6014f1576a83b8840a3ca
    Signed-off-by: kenan.liu <kenan.liu@amlogic.com>

--- libavcodec/log2_tab.c
+++ libavcodec/log2_tab.c
@@ -1,8 +1,6 @@
-#ifdef AMFFMPEG
-#include <stdint.h>
-extern uint8_t ff_log2_tab[256];
-#else
 #ifndef __ANDROID__
 #include "libavutil/log2_tab.c"
-#endif
+#else
+#include <stdint.h>
+extern uint8_t ff_log2_tab[256];
 #endif
--- libavcodec/reverse.c
+++ libavcodec/reverse.c
@@ -1,8 +1,6 @@
-#ifdef AMFFMPEG
-#include <stdint.h>
-extern uint8_t ff_reverse[256];
-#else
 #ifndef __ANDROID__
 #include "libavutil/reverse.c"
-#endif
+#else
+#include <stdint.h>
+extern uint8_t ff_reverse[256];
 #endif
